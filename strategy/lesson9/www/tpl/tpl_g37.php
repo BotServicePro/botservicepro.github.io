0d6b29919fde4637d8415de90623c34fd1ff6880
akarakin <bulik889@gmail.com>
2022-07-05 16:01:56 +0300

    folders stategy and fighting removed

--- strategy/lesson9/www/tpl/tpl_g37.php
+++
@@ -1,8 +0,0 @@
-<?   // Таверна
- echo '<br><table  style="border-collapse: collapse;" cellpadding="0" cellspacing="0" id="army"><tr>
-      <td width="450" class="tabhead">Список героев</td>
-      <tr>
-      <td width="450" class="armyinfo"><font color="#CCCCCC">В настоящий момент в деревне нет воинов, которые могли бы стать героями.</font></td>
-      <tr>
-      </table>';
-?>
